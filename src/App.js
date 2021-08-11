7edfb03c56a27746616290aa71752df3d13b8a0c
Prateek883 <singhprateek089@gmail.com>
2021-08-11 19:54:15 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { Container } from "@material-ui/core";
-import Search from "@material-ui/icons/Search";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
-import SimpleBottomNav from "./components/MainNav"
-import Series from "./pages/Series/Series";
-import Trending from "./pages/Trending/Trending";
-import Movies from "./pages/Movies/Movies";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="app"></div>
-
-      <Container>
-        <Switch>
-          <Route path="/" component={Trending} exact/>
-          <Route path="/movies" component={Movies} />
-          <Route path="/series" component={Series} />
-          <Route path="/search" component={Search} />
-        </Switch>
-      </Container>
-      <SimpleBottomNav/>
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
     </BrowserRouter>
   );
 }
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import SearchIcon from '@material-ui/icons/Search';
-import MovieIcon from '@material-ui/icons/Movie';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
-import TvIcon from '@material-ui/icons/Tv';
-
-
-
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import TvIcon from "@material-ui/icons/Tv";
+import MovieIcon from "@material-ui/icons/Movie";
+import SearchIcon from "@material-ui/icons/Search";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
     position: "fixed",
     bottom: 0,
-    backgroundColor: '#2d313a',
+    backgroundColor: "#2d313a",
     zIndex: 100,
   },
 });
@@ -23,6 +21,19 @@ const useStyles = makeStyles({
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (value === 0) {
+      history.push("/");
+    } else if (value === 1) {
+      history.push("/movies");
+    } else if (value === 2) {
+      history.push("/series");
+    } else if (value === 3) {
+      history.push("/search");
+    }
+  }, [value, history]);
 
   return (
     <BottomNavigation
@@ -33,10 +44,26 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction style={{color:"white"}} label="Trending" icon={<WhatshotIcon />} />
-      <BottomNavigationAction style={{color:"white"}} label="Movies" icon={<MovieIcon />} />
-      <BottomNavigationAction style={{color:"white"}} label="TV Series" icon={<TvIcon />} />
-      <BottomNavigationAction style={{color:"white"}} label="Search" icon={<SearchIcon />} />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
     </BottomNavigation>
   );
 }
